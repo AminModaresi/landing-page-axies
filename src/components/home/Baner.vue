7d438cf29ce36a690dcdae29b26af7820637c8f1
amin modaresi <aminiiiiiiiuii@gmail.com>
2022-03-15 11:28:40 +0330

    responsive baner section

--- src/components/home/Baner.vue
+++ src/components/home/Baner.vue
@@ -1,23 +1,22 @@
 <template>
     <section class="baner">
         <div class="container mx-auto">
-            <div class="flex justify-between">
-                <div class="basis-5/12">
-                    <div class="baner__content flex flex-col justify-center h-full">
-                        <h1 class="text-[3.3rem] font-bold mb-5">Discover, and collect extraordinary <span class="text-purple-500">Monster</span> NFTs</h1>
-                        <p class="text-gray-400 mb-5">Marketplace for Moster Character Collectons Non Fungible Token NFTs</p>
+            <div class="flex flex-wrap justify-center sm:justify-between">
+                <div class="basis-12/12 sm:basis-5/12 order-2 sm:order-1">
+                    <div class="baner__content text-center sm:text-left flex flex-col justify-center h-full">
+                        <h1 class="text-[2rem] sm:text-[1rem] md:text-[2rem] lg:text-[3.3rem] font-bold mb-5">Discover, and collect extraordinary <span class="text-purple-500">Monster</span> NFTs</h1>
+                        <p class="md:text-[.8rem] lg:text-[1rem] text-gray-400 mb-5">Marketplace for Moster Character Collectons Non Fungible Token NFTs</p>
                         <div class="btn-group">
                             <button class="btn text-sm btn-outline-purple">
                                 <FontAwesomeIcon icon="space-shuttle"/> <span class="ml-2">Explore</span>
                             </button>
                             <button class="btn text-sm ml-4 btn-outline-purple">
                                 <FontAwesomeIcon icon="cube"/> <span class="ml-2">Create</span>
-                                
                             </button>
                         </div>
                     </div>
                 </div>
-                <div class="basis-6/12">
+                <div class="basis-12/12 sm:basis-6/12 order-1 sm:order-2">
                     <img src="../../assets/images/02_hero/hero.svg" alt="">
                 </div>
             </div>
